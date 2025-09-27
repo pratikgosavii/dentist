dc8e642ea55a8a77d658c37a1310f8421bbc477c
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-09-28 02:22:08 +0530

    sdsd

--- users/signals.py
+++ users/signals.py
@@ -7,12 +7,11 @@ from .models import User
 
 # Tooth numbers for 32 teeth (exclude wisdom teeth)
 
-TOOTH_NUMBERS = [
-    "11","12","13","14","15","16","17",
-    "21","22","23","24","25","26","27",
-    "31","32","33","34","35","36","37",
-    "41","42","43","44","45","46","47"
-]
+TOOTH_NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', 
+ '9', '10', '11', '12', '13', '14', '15', '16',
+ '17', '18', '19', '20', '21', '22', '23', '24',
+ '25', '26', '27', '28', '29', '30', '31', '32']
+
 
 @receiver(post_save, sender=User)
 def create_teeth_for_customer(sender, instance, created, **kwargs):
