157b1a4c50141c5165d1fb74af0ce524e6cabcb9
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-10-04 15:18:25 +0530

    sdsd

--- doctor/models.py
+++ doctor/models.py
@@ -249,9 +249,9 @@ class AppointmentDocument(models.Model):
 
     
 class Lab(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
    
 
